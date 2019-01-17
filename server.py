c7c1a819d589cba7ab4dc938e879f98cd4b88393
homoluctus <w.slife18sy@gmail.com>
2019-01-18 05:48:57 +0900

    Add read_todo_from_db function

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from concurrent import futures
 import grpc
+import json
+from concurrent import futures
 
 
 import todo_pb2
@@ -17,12 +18,31 @@ class Todo(todo_pb2_grpc.TodoServicer):
         return task
 
 
+def read_todo_from_db(path='todo_list.json'):
+    """
+    Reads todo list from database.
+
+    Returns all task in database as a sequence of todo_pb2.Task.
+    """
+
+    todo_list = []
+
+    with open(path) as todo_list_file:
+        for item in json.load(todo_list_file):
+            task = todo_pb2.Task(id=item['id'],
+                                 name=item['name'],
+                                 is_complete=item['is_complete'],
+                                 priority=item['priority'])
+            todo_list.append(task)
+    return todo_list
+
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
     todo_pb2_grpc.add_TodoServicer_to_server(Todo(), server)
-    server.add_insecure_port('[::]:5001')
-    return
+    server.add_insecure_port('[::]:50051')
+    server.start()
 
 
 if __name__ == '__main__':
-    pass
+    serve()
