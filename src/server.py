ccb8a93756fc355ea68535d82c6351e74e4dad90
homoluctus <w.slife18sy@gmail.com>
2019-01-24 04:57:49 +0900

    Modify argument of serve() and indent

--- src/server.py
+++ src/server.py
@@ -51,22 +51,20 @@ def read_todo_from_db(db_path):
 
     todo_list = []
 
-    with open(db_path) as todo_list_file:
-        for item in json.load(todo_list_file):
-            task = todo_pb2.Task(id=item['id'],
-                                 name=item['name'],
-                                 is_complete=item['is_complete'],
-                                 priority=item['priority'])
+    with open(db_path) as db:
+        for item in json.load(db):
+            task = todo_pb2.Task(
+                        id=item['id'],
+                        name=item['name'],
+                        is_complete=item['is_complete'],
+                        priority=item['priority'])
             todo_list.append(task)
+
     return todo_list
 
 
-def serve(max_workers=5, port='[::]:50051'):
+def serve(db_path, max_workers=5, port='[::]:50051'):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
-    todo_pb2_grpc.add_TodoServicer_to_server(Todo(), server)
+    todo_pb2_grpc.add_TodoServicer_to_server(Todo(db_path), server)
     server.add_insecure_port(port)
     server.start()
-
-
-if __name__ == '__main__':
-    serve()
