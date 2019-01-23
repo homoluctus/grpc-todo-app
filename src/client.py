6a0b3338c9cb4c5db9c2c52470bd6afea766985d
homoluctus <w.slife18sy@gmail.com>
2019-01-24 04:29:40 +0900

    Move dir

---
+++ src/client.py
@@ -0,0 +1,4 @@
+import todo_pb2
+import todo_pb2_grpc
+
+
---
+++ src/server.py
@@ -0,0 +1,72 @@
+import grpc
+import json
+from concurrent import futures
+
+
+import todo_pb2
+import todo_pb2_grpc
+
+
+class Todo(todo_pb2_grpc.TodoServicer):
+    def __init__(self):
+        self.todo_list = read_todo_from_db()
+
+    def _get_task(self, key, value):
+        """
+        Find value by key
+
+        :params key: sequence key of todo_list
+        :params value: value of request sent by a user
+        """
+
+        for task in self.todo_list:
+            if task[key] == value:
+                return task
+        return None
+
+    def ListTask(self, request, context):
+        for task in self.todo_list:
+            yield task
+
+    def GetTaskById(self, request, context):
+        task = self._get_task(key='id', value=request)
+
+        if task is None:
+            return todo_pb2.GetTaskByIdResponse(task=None)
+        return task
+
+    def GetTaskByName(self, request, context):
+        task = self._get_task(key='name', value=request)
+        if task is None:
+            return todo_pb2.GetTaskByNameResponse(task=None)
+        return task
+
+
+def read_todo_from_db(path='todo_list.json'):
+    """
+    Reads todo list from database.
+
+    Returns all task in database as a sequence of todo_pb2.Task.
+    """
+
+    todo_list = []
+
+    with open(path) as todo_list_file:
+        for item in json.load(todo_list_file):
+            task = todo_pb2.Task(id=item['id'],
+                                 name=item['name'],
+                                 is_complete=item['is_complete'],
+                                 priority=item['priority'])
+            todo_list.append(task)
+    return todo_list
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
+    todo_pb2_grpc.add_TodoServicer_to_server(Todo(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+
+
+if __name__ == '__main__':
+    serve()
