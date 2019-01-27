2970137ba8a7f4dac1bc0f07338d8b99158eec4f
homoluctus <w.slife18sy@gmail.com>
2019-01-27 21:05:29 +0900

    Support to implement server and client

---
+++ todo/cli/cmd.py
@@ -0,0 +1,65 @@
+import argparse
+
+from ..client import client
+from ..server import server
+
+def handle_args():
+    parser = argparse.ArgumentParser(
+                prog='todo',
+                description='Todo server and client tool',)
+
+    subparsers = parser.add_subparsers(
+        dest='mode',
+        help='server or client mode'
+    )
+
+    server_parser = subparsers.add_parser(
+        'server',
+        help='server sub command'
+    )
+
+    client_parser = subparsers.add_parser(
+        'client',
+        help='client sub command'
+    )
+
+    handle_server_args(server_parser)
+
+    return parser.parse_args()
+
+def handle_server_args(parser):
+    parser.add_argument(
+        'path',
+        help='json file path to store data'
+    )
+
+    parser.add_argument(
+        '-w'
+        '--worker',
+        nargs='?',
+        default=5,
+        dest='worker',
+        help='Maximum workers for thread pool'
+    )
+
+    parser.add_argument(
+        '-a',
+        '--address',
+        nargs='?',
+        dest='address',
+        default='[::]:50051',
+        help='Bind address (host and port)'
+    )
+
+
+def main():
+    args = handle_args()
+
+    if getattr(args, 'mode') == 'server':
+        server.serve(
+            db_path=args.path,
+            max_workers=args.worker,
+            address=args.address
+        )
+    else:
+        client.main()
